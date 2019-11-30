85e5cf295c5b82a265932d3e5c477ff233622491
fatematujzahurarumman <50975166+fatematujzahurarumman@users.noreply.github.com>
2019-12-01 00:58:18 +0600

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-<head>
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-	<!--Footer -->
-     <div class="footer" style="margin: 0;">
-       <p style="margin: 0;">Copyright © 2019 pindo.com. All rights reserved.</p>
-     </div>
-</body>
